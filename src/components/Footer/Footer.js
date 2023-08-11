af762f3bc245d910f1d54b04a07f1ad3434690ca
Pranjul Jain <107295419+Pranjuljain1602@users.noreply.github.com>
2023-08-11 17:18:50 +0530

    React-App

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         href="https://www.youtube.com/roadsidecoder"
         style={{ cursor: "pointer" }}
       >
-        Roadside Coder
+        Pranjul Jain
       </a>
     </div>
   );
